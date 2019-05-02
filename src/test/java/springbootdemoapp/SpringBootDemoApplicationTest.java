00cc143ff8c1c0c319754a4ce600cc0fb3044d4e
acdcjunior <acdcjunior@gmail.com>
2019-05-02 04:07:12 -0300

    test: begin adding demo spring boot app

---
+++ src/test/java/springbootdemoapp/SpringBootDemoApplicationTest.java
@@ -0,0 +1,26 @@
+package springbootdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class SpringBootDemoApplicationTest {
+
+    @Autowired
+    private SomeBean someBean;
+
+    @Test
+    @Sql
+    void contextLoads() {
+        assertThat(someBean.getSomething()).isEqualTo("something!");
+    }
+
+}
