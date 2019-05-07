a1693f9c617cfce827fb4e8f907eab2ecf35752f
acdcjunior <acdcjunior@gmail.com>
2019-05-07 19:27:10 -0300

    feat(usertype): add new hibernate automatic usertype registration

---
+++ src/main/java/io/github/acdcjunior/domainid/hibernate/usertype/DomainIdJavaDescriptor.java
@@ -0,0 +1,48 @@
+package io.github.acdcjunior.domainid.hibernate.usertype;
+
+import io.github.acdcjunior.domainid.DomainId;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.AbstractTypeDescriptor;
+import org.hibernate.type.descriptor.java.ImmutableMutabilityPlan;
+
+
+public class DomainIdJavaDescriptor<T extends DomainId> extends AbstractTypeDescriptor<T> {
+
+    private final Class<T> clazz;
+
+    @SuppressWarnings("unchecked")
+    public DomainIdJavaDescriptor(Class<T> clazz) {
+        super(clazz, ImmutableMutabilityPlan.INSTANCE);
+        this.clazz = clazz;
+    }
+
+    @Override
+    public String toString(T value) {
+        return value.toLong() + "";
+    }
+
+    @Override
+    public T fromString(String string) {
+        return DomainId.newInstance(clazz, Long.valueOf(string));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <X> X unwrap(T value, Class<X> type, WrapperOptions options) {
+        if (value == null) { return null; }
+        if (DomainId.class.isAssignableFrom(type)) { return (X) value; }
+        if (Long.class.isAssignableFrom(type)) { return (X) (Long) value.toLong(); }
+        throw unknownUnwrap(type);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <X> T wrap(X value, WrapperOptions options) {
+        if (value == null) { return null; }
+        if (value instanceof Long && (Long) value == 0) { return null; }
+        if (value instanceof DomainId) { return (T) value; }
+        if (value instanceof Long) { return DomainId.newInstance(clazz, (Long) value); }
+        throw unknownWrap(value.getClass());
+    }
+
+}
---
+++ src/main/java/io/github/acdcjunior/domainid/hibernate/usertype/DomainIdType.java
@@ -0,0 +1,33 @@
+package io.github.acdcjunior.domainid.hibernate.usertype;
+
+import io.github.acdcjunior.domainid.DomainId;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.type.AbstractSingleColumnStandardBasicType;
+import org.hibernate.type.LiteralType;
+import org.hibernate.type.descriptor.sql.BigIntTypeDescriptor;
+
+
+public class DomainIdType<T extends DomainId>
+		extends AbstractSingleColumnStandardBasicType<T>
+		implements LiteralType<T> {
+
+    private final Class<T> clazz;
+
+    public DomainIdType(Class<T> clazz) {
+		super( BigIntTypeDescriptor.INSTANCE, new DomainIdJavaDescriptor<>(clazz) );
+		this.clazz = clazz;
+	}
+
+    @Override
+	public String getName() {
+		return clazz.getSimpleName();
+	}
+
+	@Override
+	protected boolean registerUnderJavaType() {
+		return true;
+	}
+
+	@Override public String objectToSQLString(T value, Dialect dialect) { return "'" + value.toLong() + "'"; }
+
+}
