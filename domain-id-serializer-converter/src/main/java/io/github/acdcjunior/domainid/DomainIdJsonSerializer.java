ef2a5deb531155e5c58939df3fba79ff390cf394
acdcjunior <acdcjunior@gmail.com>
2019-05-27 06:22:45 -0300

    feat: remove deserializer (was not being used) and create LinkedDomainId

---
+++ domain-id-serializer-converter/src/main/java/io/github/acdcjunior/domainid/DomainIdJsonSerializer.java
@@ -0,0 +1,95 @@
+package io.github.acdcjunior.domainid;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@JsonComponent
+@Component
+public class DomainIdJsonSerializer extends JsonSerializer<DomainId> implements ContextualSerializer {
+
+    private RequiredPlaceholderResolver environment = (s) -> s;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment::resolveRequiredPlaceholders;
+    }
+
+    private interface RequiredPlaceholderResolver {
+        String resolveRequiredPlaceholders(String text) throws IllegalArgumentException;
+    }
+
+    @Override
+    public void serialize(DomainId domainId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeNumber(domainId.toLong());
+    }
+
+    @Override
+    public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) {
+        if (property == null) {
+            return this;
+        }
+        LinkedDomainId linkedDomainId = property.getType().getRawClass().getAnnotation(LinkedDomainId.class);
+        if (linkedDomainId == null) {
+            return this;
+        }
+        String href = environment.resolveRequiredPlaceholders(linkedDomainId.value());
+        if (Arrays.asList("id", "cod").contains(property.getName())) {
+            return new DomainIdAsObjectSerializers.UnwrappedDomainIdSerializer(href);
+        }
+        return new DomainIdAsObjectSerializers.WrappedDomainIdSerializer(href);
+    }
+
+}
+
+class DomainIdAsObjectSerializers {
+
+    private static void writeLinksSelfHrefField(JsonGenerator jsonGenerator, String value) throws IOException {
+        Map<String, Object> _links = new HashMap<>();
+        Map<String, Object> self = new HashMap<>();
+        self.put("href", value);
+        _links.put("self", self);
+        jsonGenerator.writeObjectField("_links", _links);
+    }
+
+    static class WrappedDomainIdSerializer extends JsonSerializer<DomainId> {
+        private final String href;
+        WrappedDomainIdSerializer(String href) {
+            this.href = href;
+        }
+        @Override
+        public void serialize(DomainId domainId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeStartObject();
+            jsonGenerator.writeNumberField("id", domainId.toLong());
+            writeLinksSelfHrefField(jsonGenerator, href.replace("#", domainId.toString()));
+            jsonGenerator.writeEndObject();
+        }
+    }
+
+    static class UnwrappedDomainIdSerializer extends JsonSerializer<DomainId> {
+        private final String href;
+        UnwrappedDomainIdSerializer(String href) {
+            this.href = href;
+        }
+        @Override
+        public void serialize(DomainId domainId, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeNumber(domainId.toLong());
+            writeLinksSelfHrefField(jsonGenerator, href.replace("#", domainId.toString()));
+        }
+        @Override
+        public boolean isUnwrappingSerializer() {
+            return true;
+        }
+    }
+}
---
+++ domain-id-serializer-converter/src/main/java/io/github/acdcjunior/domainid/LinkedDomainId.java
@@ -0,0 +1,25 @@
+package io.github.acdcjunior.domainid;
+
+import java.lang.annotation.*;
+
+
+/**
+ * Configures the DomainId class to be serialized as an object with id and _links.self.href properties.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LinkedDomainId {
+
+    /**
+     * <p>The href for the self link. Can be a Spring Expression.</p>
+     * <p>If there is a {@code #} character in the string, it will be replaced with the actual id value.</p>
+     * Example:
+     * <br>
+     *     {@code @LinkedDomainId("${MY_API.BASE_URL}/api/resources/#")}
+     * <br>
+     *     becomes {@code "http://the-value-for-that-base-url-spring-prop.com/api/resources/123"}
+     */
+    String value();
+
+}
+
