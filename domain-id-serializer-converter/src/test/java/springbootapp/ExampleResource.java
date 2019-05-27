d209d7efeeb95ccb999cc150ab179ec6a6632889
acdcjunior <acdcjunior@gmail.com>
2019-05-27 04:08:41 -0300

    test: add other field to ExampleResource

--- domain-id-serializer-converter/src/test/java/springbootapp/ExampleResource.java
+++ domain-id-serializer-converter/src/test/java/springbootapp/ExampleResource.java
@@ -1,16 +1,15 @@
 package springbootapp;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ExampleResource {
 
     private ExampleResourceId id;
     private String name;
+    private ExampleResourceId otherResource;
 
     public ExampleResourceId getId() {
         return id;
     }
-
     public void setId(ExampleResourceId id) {
         this.id = id;
     }
@@ -18,10 +17,15 @@ public class ExampleResource {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
 
+    public ExampleResourceId getOtherResource() {
+        return otherResource;
+    }
+    public void setOtherResource(ExampleResourceId otherResource) {
+        this.otherResource = otherResource;
+    }
 
 }
